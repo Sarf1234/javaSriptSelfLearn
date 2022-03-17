ce061f41dafaf7cb2c0eac3837b60a1d6bbc1b63
Sarf1234 <sarfarajshekh69@gmail.com>
2022-03-17 22:57:30 +0530

    assingment solve

--- loopsAndAssingment.js
+++ loopsAndAssingment.js
@@ -54,4 +54,57 @@ for(let i = 0; i <= 5; i++){
 
   //                          start exercises
 
-  
+  const countreis = ['Albania','Bolivia','Canada','Denmark','Ethiopia','Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'];
+
+  for(const number of countreis){
+      console.log(number);
+  }
+  let n = 5;
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      string += "#";
+    }
+    string += "\n";
+  }
+  console.log(string);
+
+
+  //Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+  let sum = 0;
+  for(let i = 0; i <= 100; i++){
+      sum = sum + i;
+  }
+  console.log(sum);
+
+  //Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+let  even = 0;
+for(let i = 1; i <= 100; i++){
+    if(i % 2 ==0){
+        even = even + i;
+    }
+}
+console.log(even);
+
+let odd = 0;
+for (let i = 0; i <= 100; i++){
+    odd += i;
+}
+console.log(odd);
+
+
+// Develop a small script which generate array of 5 random numbers
+
+
+let araay = [1,2,3,4,5,6,7,8,9];
+let stng = ""
+for(let i = 0; i < 5; i++){
+      stng += araay[Math.floor(Math.random(araay[i]) * 6)];
+}
+console.log(stng);
