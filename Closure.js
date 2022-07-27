9a525f668aab0fc1c0f62c90afa23574873edb28
Sarf1234 <sarfarajshekh69@gmail.com>
2022-07-27 14:37:46 +0530

    closure

---
+++ Closure.js
@@ -0,0 +1,35 @@
+/* Closure =>   A function having access 
+                to the parent scope, even after 
+                the parent function has closed.
+
+              OR
+  
+ closure is  bundle of function along with scope is 
+ called closure 
+ it mean like if you have a function in a function 
+ then the inner function use parent function variable
+ it has a scope also of parent function.
+
+ 
+
+*/
+
+
+function x(){
+    let a = 10;
+    function y(){
+        console.log(a);
+    }
+    y();
+}
+
+x();
+
+ //  uses of closure 
+
+ // 1. module desing pattern
+ // 2. currying 
+ // 3. function line once
+ // 4. memoize
+ // 5. maintaining state in async world
+ // 6. setTiomeouts
