ca0ba9372db2107e044ec5a5ed45946699aef0f9
Sarf1234 <sarfarajshekh69@gmail.com>
2022-07-29 23:06:48 +0530

    call back

---
+++ CallbacksAndeventListners.js
@@ -0,0 +1,13 @@
+// CallBack => when we pass a function inside a function is call
+//             callback function.
+
+
+//                   or
+
+// when we pass a function as a parameter in a function is called 
+// callback function.
+
+let btn = document.getElementById('clickme');
+btn.addEventListener('click', () =>{
+    console.log("click me")
+})
