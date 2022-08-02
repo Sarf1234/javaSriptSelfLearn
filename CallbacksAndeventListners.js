c55d7a22abb1c06652e8f33f46be26026a5dd678
Sarf1234 <sarfarajshekh69@gmail.com>
2022-08-02 21:10:39 +0530

    commit for highe order function

--- CallbacksAndeventListners.js
+++ CallbacksAndeventListners.js
@@ -13,12 +13,21 @@
 // })
 
 
-let newbtn = document.querySelector('.toggle');
+// let newbtn = document.querySelector('.toggle');
 
 
-newbtn.addEventListener('click', toggleColor)
-function toggleColor(){
-    newbtn.classList.toggle('toggle-blue')
+// newbtn.addEventListener('click', toggleColor)
+// function toggleColor(){
+//     newbtn.classList.toggle('toggle-blue')
+// }
+
+
+function CallBackA(massage){
+    console.log("Hey i am A");
+    setTimeout(massage, 2000)
+
 }
-
-
+function CallBackB(){
+    console.log("Hey i am B")
+}
+setTimeout(() =>CallBackA(CallBackB), 3000);
--- HigherOrderFunction.js
+++ HigherOrderFunction.js
@@ -1,23 +1,45 @@
-let array = [1, 2, 3, 4 , 5 , 6];
+let arr = [1, 2, 3, 4 , 5 , 6];
 
 // Higher order function => when we pass a function in a function as a parameter
 //                           and a function return a fucntion is called higher order
 //                           function.
 
+//               MAP
 
 
-function A(B){
-          console.log("Heigher order function here");
-          B();
-     
-}
+let mapoutput =  arr.map(double =>{
+        return double * 2;
+})
 
-function B(){
-    console.log("call back function " )
-}
-function C(){
-    console.log("Here is the normal function")
-}
+console.log(mapoutput);
 
-A(B);
-C();
+//               Filter
+
+let filterOutput = arr.filter(lessthen =>{
+    return lessthen > 5;
+})
+
+console.log(filterOutput);
+
+//              Reduce
+
+//find the sum of array
+
+
+let sum = arr.reduce((acc, cur) =>{
+    acc = acc + cur;
+    return acc;
+})
+
+console.log(sum)
+
+// find largest number is array
+
+let largestNumber = arr.reduce((max, cur) =>{
+    if(max < cur){
+        max = cur;
+    }
+    return max;
+})
+
+console.log(largestNumber);
